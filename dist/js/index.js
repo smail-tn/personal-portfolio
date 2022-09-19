2fa6745a927a85b09509ccf1079d2380f11e751e
web creative <smailettanjaoui@gmail.com>
2022-09-19 15:18:03 +0100

    " "

--- dist/js/index.js
+++ dist/js/index.js
@@ -17,141 +17,148 @@ let Body = document.body,
   querySelectorAll = (e) => document.querySelectorAll(e);
 //-----------------------
 
-function toggleNavbar() {
-  let collapse = querySelector(".header .nav-collapse");
-  let navToggler = querySelector(".header .nav-toggler");
+window.addEventListener("DOMContentLoaded", function () {
+  function toggleNavbar() {
+    let collapse = querySelector(".header .nav-collapse");
+    let navToggler = querySelector(".header .nav-toggler");
 
-  navToggler.addEventListener("click", function () {
-    if (!collapse.classList.contains("open")) {
-      collapse.style.maxHeight = collapse.scrollHeight + "px";
-      collapse.classList.add("open");
+    navToggler.addEventListener("click", function () {
+      if (!collapse.classList.contains("open")) {
+        collapse.style.maxHeight = collapse.scrollHeight + "px";
+        collapse.classList.add("open");
+      } else {
+        collapse.removeAttribute("style");
+        collapse.classList.remove("open");
+      }
+    });
+
+    querySelectorAll(".nav-link").forEach(
+      (el) =>
+        (el.onclick = () => {
+          collapse.removeAttribute("style");
+          collapse.classList.remove("open");
+        })
+    );
+  }
+  //  
+  document.addEventListener('click', function (e) { 
+    if (e.target == document && e.target !== navToggler && e.target !== collapse) { 
+  collapse.classList.removeAttribute('style')
+  }
+  })
+
+  toggleNavbar();
+
+  window.addEventListener("scroll", function () {
+    let header = querySelector(".home .header");
+    if (this.scrollY > 20) {
+      header.classList.add("active");
     } else {
-      collapse.removeAttribute("style");
-      collapse.classList.remove("open");
+      header.classList.remove("active");
     }
   });
 
-  querySelectorAll(".nav-link").forEach(
-    (el) =>
-      (el.onclick = () => {
-        collapse.removeAttribute("style");
-        collapse.classList.remove("open");
-      })
-  );
-}
+  function ActiveLinks() {
+    let secs = querySelectorAll(".section");
 
-toggleNavbar();
-
-window.addEventListener("scroll", function () {
-  let header = querySelector(".home .header");
-  if (this.scrollY > 20) {
-    header.classList.add("active");
-  } else {
-    header.classList.remove("active");
-  }
-});
-
-function ActiveLinks() {
-  let secs = querySelectorAll(".section");
-
-  window.addEventListener("scroll", function (e) {
-    secs.forEach((sec) => {
-      let id = sec.id;
-      let top = sec.offsetTop - 100;
-      let scroll = window.scrollY;
-      if (scroll > top && scroll < top + sec.clientHeight) {
-        querySelectorAll(".header .nav-link").forEach((link) =>
-          link.classList.remove("active")
-        );
-        querySelector(`.nav-link[href="#${id}"]`).classList.add("active");
-      }
-    });
-  });
-}
-
-ActiveLinks();
-
-function gallery() {
-  let titles = querySelectorAll(".gallery .gallery-header li[data-type]");
-  let boxs = querySelectorAll(".gallery .images .box");
-  titles[0].previousElementSibling.onclick = function () {
-    boxs.forEach((box) => (box.style.display = "block"));
-    titles.forEach((el) => el.classList.remove("active"));
-    this.classList.add("active");
-  };
-  titles.forEach((title) => {
-    title.addEventListener("click", function () {
-      // remove add active class
-      titles.forEach((title) => {
-        title.classList.remove("active");
-        titles[0].previousElementSibling.classList.remove("active");
-      });
-      this.classList.add("active");
-      // show hide box
-
-      let type = this.dataset.type;
-
-      boxs.forEach((box, i) => {
-        if (box.dataset.type !== type) {
-          box.style.display = "none";
-        } else {
-          box.style.display = "block";
+    window.addEventListener("scroll", function (e) {
+      secs.forEach((sec) => {
+        let id = sec.id;
+        let top = sec.offsetTop - 100;
+        let scroll = window.scrollY;
+        if (scroll > top && scroll < top + sec.clientHeight) {
+          querySelectorAll(".header .nav-link").forEach((link) =>
+            link.classList.remove("active")
+          );
+          querySelector(`.nav-link[href="#${id}"]`).classList.add("active");
         }
       });
     });
-  });
-  //
-  let prev = document.querySelector(".gallery .prev");
-  let next = document.querySelector(".gallery .next");
-  let preview = document.querySelector(".gallery-preview");
-  let showAlt = preview.querySelector(".alt");
-  let showNumber = preview.querySelector(".number");
-  let showImage = preview.querySelector(".space img");
-  let close = preview.querySelector(".close");
-
-  let count = boxs.length;
-  let index = 0;
-
-  showImage.onclick = () => next.click();
-
-  boxs.forEach((box, i) => {
-    close.onclick = () => {
-      preview.classList.remove("show");
-      pageOverlay.classList.remove("show");
-    };
-    box.addEventListener("click", function () {
-      pageOverlay.classList.add("show");
-      preview.classList.add("show");
-
-      //
-      change(i);
-      index = i;
-      prev.onclick = function () {
-        if (index == 0) index = count - 1;
-        else index--;
-        change(index);
-      };
-      next.onclick = function () {
-        if (index == count - 1) index = 0;
-        else index++;
-        change(index);
-      };
-    });
-  });
-  let pageOverlay = document.querySelector(".page-overlay");
-
-  function change(index) {
-    let src = boxs[index].querySelector("img").getAttribute("src");
-    let alt = boxs[index].querySelector("img").getAttribute("alt");
-    showImage.src = src;
-    showAlt.textContent = alt;
-    showNumber.textContent = `${index + 1}/${count}`;
   }
-  preview.addEventListener("click", function (e) {
-    if (e.target == preview) {
-      close.click();
-    }
-  });
-}
 
-gallery();
+  ActiveLinks();
+
+  function gallery() {
+    let titles = querySelectorAll(".gallery .gallery-header li[data-type]");
+    let boxs = querySelectorAll(".gallery .images .box");
+    titles[0].previousElementSibling.onclick = function () {
+      boxs.forEach((box) => (box.style.display = "block"));
+      titles.forEach((el) => el.classList.remove("active"));
+      this.classList.add("active");
+    };
+    titles.forEach((title) => {
+      title.addEventListener("click", function () {
+        // remove add active class
+        titles.forEach((title) => {
+          title.classList.remove("active");
+          titles[0].previousElementSibling.classList.remove("active");
+        });
+        this.classList.add("active");
+        // show hide box
+
+        let type = this.dataset.type;
+
+        boxs.forEach((box, i) => {
+          if (box.dataset.type !== type) {
+            box.style.display = "none";
+          } else {
+            box.style.display = "block";
+          }
+        });
+      });
+    });
+    //
+    let prev = document.querySelector(".gallery .prev"),
+      pageOverlay = document.querySelector(".page-overlay"),
+      next = document.querySelector(".gallery .next"),
+      preview = document.querySelector(".gallery-preview"),
+      showAlt = preview.querySelector(".alt"),
+      showNumber = preview.querySelector(".number"),
+      showImage = preview.querySelector(".space img"),
+      close = preview.querySelector(".close"),
+      count = boxs.length,
+      index = 0;
+
+    showImage.onclick = () => next.click();
+
+    boxs.forEach((box, i) => {
+      close.onclick = () => {
+        preview.classList.remove("show");
+        pageOverlay.classList.remove("show");
+      };
+      box.addEventListener("click", function () {
+        pageOverlay.classList.add("show");
+        preview.classList.add("show");
+
+        //
+        change(i);
+        index = i;
+        prev.onclick = function () {
+          if (index == 0) index = count - 1;
+          else index--;
+          change(index);
+        };
+        next.onclick = function () {
+          if (index == count - 1) index = 0;
+          else index++;
+          change(index);
+        };
+      });
+    });
+
+    function change(index) {
+      let src = boxs[index].querySelector("img").getAttribute("src");
+      let alt = boxs[index].querySelector("img").getAttribute("alt");
+      showImage.src = src;
+      showAlt.textContent = alt;
+      showNumber.textContent = `${index + 1}/${count}`;
+    }
+    preview.addEventListener("click", function (e) {
+      if (e.target == preview) {
+        close.click();
+      }
+    });
+  }
+
+  gallery();
+}); // end of DomContentLoaded
